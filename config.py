a4eb3d9859dc69b73f1298d1c6505f089c1117bc
ARYAN x SHOAIB <shoaib910385@gmail.com>
2026-07-11 10:19:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ UPSTREAM_REPO = getenv(
     "https://github.com/shoaib910385/valrikmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GIT_TOKEN = "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"  # Fill this variable if your upstream repository is private
+GIT_TOKEN = ""  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/+8JOfBn0H5RoxMzk9")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Adsu_x4")
